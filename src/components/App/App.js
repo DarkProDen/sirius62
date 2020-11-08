6e18af694065483376e781535c64557575e82284
Denis Vilkov <denisvilkov666@gmail.com>
2020-11-08 06:51:09 +0300

    feat: add documents

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import CompetitionThemes from "../CompetitionThemes/CompetitionThemes";
-import Header from "../Header/Header";
+import React from 'react';
+import CompetitionThemes from '../CompetitionThemes/CompetitionThemes';
+import Header from '../Header/Header';
+import Documents from '../Documents/Documents';
 
 function App() {
   return (
     <div>
       <Header />
       <CompetitionThemes />
+      <Documents />
     </div>
   );
 }
