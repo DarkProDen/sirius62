4bdac8effc12c5249fdac728a2586589f8a3f620
Denis Vilkov <denisvilkov666@gmail.com>
2020-11-08 20:31:12 +0300

    feat: comment links

--- src/components/Documents/Documents.js
+++ src/components/Documents/Documents.js
@@ -10,27 +10,27 @@ function Documents() {
   return (
     <div className="documents">
       <div className="document-card">
-        <a target="blank" href={polojenie}>
-          <div className="document-card__icon far fa-file-alt"></div>
-        </a>
+        {/* <a target="blank" href={polojenie}>*/}
+        <div className="document-card__icon far fa-file-alt"></div>
+        {/* </a>*/}
         <h2 className="document-card__text">Положение конкурса</h2>
       </div>
       <div className="document-card">
-        <a target="blank" href={prikaz}>
-          <div className="document-card__icon far fa-edit"></div>
-        </a>
+        {/* <a target="blank" href={prikaz}>*/}
+        <div className="document-card__icon far fa-edit"></div>
+        {/* </a>*/}
         <h2 className="document-card__text">Приказ о конкурсе</h2>
       </div>
       <div className="document-card">
-        <a target="blank" href={sostavKomiteta}>
-          <div className="document-card__icon far fa-address-book"></div>
-        </a>
+        {/* <a target="blank" href={sostavKomiteta}>*/}
+        <div className="document-card__icon far fa-address-book"></div>
+        {/* </a>*/}
         <h2 className="document-card__text">Состав Организационного комитета</h2>
       </div>
       <div className="document-card">
-        <a target="blank" href={sostavKomissy}>
-          <div className="document-card__icon far fa-address-card"></div>
-        </a>
+        {/* <a target="blank" href={sostavKomissy}>*/}
+        <div className="document-card__icon far fa-address-card"></div>
+        {/* </a>*/}
         <h2 className="document-card__text">Состав Экспертной комиссии очного/заочного этапа</h2>
       </div>
     </div>
