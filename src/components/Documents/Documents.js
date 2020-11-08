5671ef8f980325d1951da48b8ed38d9514fd07a1
Denis Vilkov <denisvilkov666@gmail.com>
2020-11-08 20:15:16 +0300

    feat: add uploads

--- src/components/Documents/Documents.js
+++ src/components/Documents/Documents.js
@@ -1,45 +1,34 @@
 import React from 'react';
 import './Documents.css';
 import './DocumentsMobile.css';
-
-//<i class="far fa-edit"></i>
-//<i class="far fa-address-book"></i>
+import polojenie from '../../uploads/polojenie-bolshie-vizovy.pdf';
+import prikaz from '../../uploads/prikaz-bolshie-vizovy.pdf';
+import sostavKomiteta from '../../uploads/Sostav_Organizatsionnogo_Komiteta_Regionalnogo_Treka_Konkursa.pdf';
+import sostavKomissy from '../../uploads/Sostav_Expertnykh_Komissy_Regionalnogo_Treka_Konkursa.pdf';
 
 function Documents() {
   return (
     <div className="documents">
       <div className="document-card">
-        <a
-          target="blank"
-          href="https://sirius62.ru/wp-content/uploads/2019/11/%D0%9F%D0%BE%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D0%91%D0%BE%D0%BB%D1%8C%D1%88%D0%B8%D0%B5-%D0%B2%D1%8B%D0%B7%D0%BE%D0%B2%D1%8B.pdf"
-        >
+        <a target="blank" href={polojenie}>
           <div className="document-card__icon far fa-file-alt"></div>
         </a>
         <h2 className="document-card__text">Положение конкурса</h2>
       </div>
       <div className="document-card">
-        <a
-          target="blank"
-          href="https://sirius62.ru/wp-content/uploads/2019/11/%D0%9F%D1%80%D0%B8%D0%BA%D0%B0%D0%B7-%D0%91%D0%BE%D0%BB%D1%8C%D1%88%D0%B8%D0%B5-%D0%B2%D1%8B%D0%B7%D0%BE%D0%B2%D1%8B.pdf"
-        >
+        <a target="blank" href={prikaz}>
           <div className="document-card__icon far fa-edit"></div>
         </a>
         <h2 className="document-card__text">Приказ о конкурсе</h2>
       </div>
       <div className="document-card">
-        <a
-          target="blank"
-          href="http://sirius62.ru/wp-content/uploads/2020/03/Sostav_Organizatsionnogo_Komiteta_Regionalnogo_Treka_Konkursa.pdf"
-        >
+        <a target="blank" href={sostavKomiteta}>
           <div className="document-card__icon far fa-address-book"></div>
         </a>
         <h2 className="document-card__text">Состав Организационного комитета</h2>
       </div>
       <div className="document-card">
-        <a
-          target="blank"
-          href="http://sirius62.ru/wp-content/uploads/2020/03/Sostav_Expertnykh_Komissy_Regionalnogo_Treka_Konkursa.pdf"
-        >
+        <a target="blank" href={sostavKomissy}>
           <div className="document-card__icon far fa-address-card"></div>
         </a>
         <h2 className="document-card__text">Состав Экспертной комиссии очного/заочного этапа</h2>
