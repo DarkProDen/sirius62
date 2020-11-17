bd909f7bd01f072647e236aedbba301eb20c17e2
Денис Вилков <-g>
2020-11-17 20:28:37 +0300

    feat: add Timing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,13 @@ import CompetitionThemes from '../CompetitionThemes/CompetitionThemes';
 import Header from '../Header/Header';
 import Documents from '../Documents/Documents';
 import Contacts from '../Contacts/Contacts';
+import Timing from '../Timing/Timing';
 
 function App() {
   return (
     <div>
       <Header />
+      <Timing />
       <CompetitionThemes />
       <Documents />
       <Contacts />
---
+++ src/components/Timing/Timing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Timing.css';
+
+function Timing() {
+  return (
+    <div className="timing">
+      Прием заявок на региональный трек Конкурса "Большие вызовы" до 15 января
+      2021 г.
+    </div>
+  );
+}
+
+export default Timing;
