2bdef20645a79821378d6cd4c0eeeef9b7b465b6
Denis Vilkov <denisvilkov666@gmail.com>
2021-04-01 14:10:32 +0300

    feat: add rezultati_ochnogo_etapa.pdf

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,16 @@ import Header from '../Header/Header';
 import Documents from '../Documents/Documents';
 import Contacts from '../Contacts/Contacts';
 import Timing from '../Timing/Timing';
-import spisokPrizerov from '../../uploads/Prizeri_ochnogo_etapa.pdf';
+import results from '../../uploads/rezultati_ochnogo_etapa.pdf';
 
 function App() {
   return (
     <>
       <Header />
       <Timing>
-        <a href={spisokPrizerov} target="_blank" rel="noopener noreferrer">
-          Список победителей и призеров очного этапа Регионального трека Всероссийского конкурса
-          научно-технологических проектов «Большие вызовы»
+        <a href={results} target="_blank" rel="noopener noreferrer">
+          Результаты очного этапа Регионального трека Всероссийского конкурса научно-технологических
+          проектов «Большие вызовы»
         </a>
       </Timing>
       <CompetitionThemes />
