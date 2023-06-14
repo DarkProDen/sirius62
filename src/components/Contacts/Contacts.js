728446c64101fa3e8d60e5f6144269de3f47e94a
Denis Vilkov <denisvilkovwork@gmail.com>
2023-06-14 12:50:02 +0300

    refactor: mask phone

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -9,7 +9,7 @@ function CompetitionThemes() {
       <div className="contacts-wrap">
         <div className="contact-card">
           <h4>Кочеткова Марина Петровна</h4>
-          <div className="contact-card__phone-number">Телефон: 8 (4912) 51-51-52 (доб. 310)</div>
+          <div className="contact-card__phone-number">Телефон: 8 (4912) xx-xx-xx (доб. 310)</div>
           <div className="contact-card__description">
             Начальник отдела социализации и развития одаренности детей министерства образования
             Рязанской области
@@ -17,7 +17,7 @@ function CompetitionThemes() {
         </div>
         <div className="contact-card">
           <h4>Викулова Мария Геннадьевна</h4>
-          <div className="contact-card__phone-number">Телефон: 8 (4912) 55-92-80</div>
+          <div className="contact-card__phone-number">Телефон: 8 (4912) xx-xx-xx</div>
           <div className="contact-card__description">
             Заместитель директора Областного государственного автономного учреждения дополнительного
             образования «Детский технопарк Кванториум «Дружба»
